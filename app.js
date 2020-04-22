7ecbc8b0ef1fd75138c9445026af8715b7bb6c97
ananda <anandawira754@gmail.com>
2020-04-22 08:57:53 -0700

    initialized node.js and added some dependencies

---
+++ app.js
@@ -0,0 +1,7 @@
+import "./src/script/component/app-bar.js";
+import "./src/script/component/movie-item.js";
+import "./src/script/component/movie-list.js";
+import "./src/script/component/copyright.js";
+import main from "./src/script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);

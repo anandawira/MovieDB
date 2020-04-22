e928a3ef8670c6b19881d1c37773fb7deb2d3778
ananda <anandawira754@gmail.com>
2020-04-22 09:31:14 -0700

    added webpack configs

--- app.js
+++
@@ -1,7 +0,0 @@
-import "./src/script/component/app-bar.js";
-import "./src/script/component/movie-item.js";
-import "./src/script/component/movie-list.js";
-import "./src/script/component/copyright.js";
-import main from "./src/script/view/main.js";
-
-document.addEventListener("DOMContentLoaded", main);
